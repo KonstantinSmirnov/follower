5d760947d0022fc335fc5e674f0e09b344350676
konstantinsmirnov <k.smi@mail.ru>
2017-03-06 21:59:18 +0300

    Add styling

--- spec/features/widget/widget_appearance_spec.rb
+++ spec/features/widget/widget_appearance_spec.rb
@@ -12,6 +12,10 @@ feature 'WIDGET APPEARANCE' do
       expect(page).to have_selector('h4', text: 'Follower')
     end
 
+    scenario 'widget has params header', js: true do
+      expect(page).to have_selector('#follower_widget__params_header', text: 'Parameters')
+    end
+
     scenario 'widget can be hidden and unhidden', js: true do
       expect(page).to have_selector('#follower_widget__collapse_button', text: '>')
       expect(find("#follower_widget__root").native.css_value('right')).to eq('0px')
